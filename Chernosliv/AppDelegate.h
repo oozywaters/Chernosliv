756a6292cf71281ba7ad953c8db3b206c45e1be2
oozywaters <oozy.waters@gmail.com>
2015-07-11 12:10:29 +1000

    Initial Commit

---
+++ Chernosliv/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chernosliv
+//
+//  Created by Vacheslav Zavertanny on 11.07.15.
+//  Copyright (c) 2015 Vyacheslav Zavertanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chernosliv/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chernosliv
+//
+//  Created by Vacheslav Zavertanny on 11.07.15.
+//  Copyright (c) 2015 Vyacheslav Zavertanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
