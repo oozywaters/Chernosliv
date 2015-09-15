f487b8909a0620f28a643769f1f9963e16a9a43f
oozywaters <oozy.waters@gmail.com>
2015-09-15 21:54:08 +1000

    Add comments cell

---
+++ Chernosliv/Modules/Comments/UserInterface/View/Cells/MKCCommentsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MKCCommentsTableViewCell.h
+//  Chernosliv
+//
+//  Created by Vyacheslav Zavertanny on 15/09/15.
+//  Copyright (c) 2015 Vyacheslav Zavertanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKCCommentsTableViewCell : UITableViewCell
+
+@end
---
+++ Chernosliv/Modules/Comments/UserInterface/View/Cells/MKCCommentsTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MKCCommentsTableViewCell.m
+//  Chernosliv
+//
+//  Created by Vyacheslav Zavertanny on 15/09/15.
+//  Copyright (c) 2015 Vyacheslav Zavertanny. All rights reserved.
+//
+
+#import "MKCCommentsTableViewCell.h"
+
+@implementation MKCCommentsTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
